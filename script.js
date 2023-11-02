a3587641af3d669363890c71696c1b406e9e90c6
Vishal Gupta <vkg051198@gmail.com>
2023-11-02 15:39:43 +0530

    Personal Website

---
+++ script.js
@@ -0,0 +1,8 @@
+// auto text animation
+let typed = new Typed(".auto-text", {
+    strings:["Frontend Developer.", "Web Developer.", "Java Developer."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
